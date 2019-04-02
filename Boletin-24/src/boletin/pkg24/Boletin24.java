37d92cddfa3263b57ff8a53632af71c839fd0103
ifernandezblanco <ifernandezblanco@nautilus29.danielcastelao.org>
2019-04-02 12:02:23 +0200

    Pro-Boletin24 revisado

--- Boletin-24/src/boletin/pkg24/Boletin24.java
+++ Boletin-24/src/boletin/pkg24/Boletin24.java
@@ -9,8 +9,10 @@ public class Boletin24 {
 
     
     public static void main(String[] args) {
-       Ventana obxVen = new Ventana();
-       obxVen.crearVentana();
+//       Ventana obxVen = new Ventana();
+//       obxVen.crearVentana();
+Ventana2 obxVent2 = new Ventana2();
+obxVent2.setVisible(true);
        
     }
     
--- Boletin-24/src/boletin/pkg24/Ventana.java
+++ Boletin-24/src/boletin/pkg24/Ventana.java
@@ -1,82 +1,128 @@
 
 package boletin.pkg24;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
  *
  * @author ifernandezblanco
  */
-public class Ventana {
+public class Ventana implements ActionListener {
     //creamos los componentes
-    JFrame marco;
-    JPanel panel, panel2, panel3;
-    JButton boton, boton2, boton3;
-    JLabel etiqueta, etiqueta2;
-    JTextField textoNome;
-    JPasswordField contraseña;
-    JTextArea areaTexto;
-    JList lista;
-    //hay que rediseñarlo casi por completo
-    //hacer que los botones hagan algo 
-    //escribir y darle al boton "boton" y que se escriba todo en el area de texto
-    
-    public void crearVentana(){
-        //instancia de los objetos creados
-        marco = new JFrame("Ventanuco");
+       JFrame marco;
+    JPanel panel, panel1, panel2;
+    JLabel Nome, Password;
+    JTextField NomeText;
+    JPasswordField Passwordtext;
+    JButton bPremer, bLimpar, boton;
+    JTextArea atComentarios;
+    JList Items;
+    String[] items = {"DAM1", "DAM2", "ASIR1", "ASIR2"};
+
+   
+    public void instanciarComponentesVentana() {
+
+        //inicializar
+        marco = new JFrame("Ventana");
         panel = new JPanel();
-        panel2= new JPanel();
-        panel3 = new JPanel();
-        boton = new JButton("Premer");
-        boton2 = new JButton("Limpar");
-        boton3 = new JButton("boton");
-        etiqueta = new JLabel("");
-        etiqueta2 = new JLabel("");
-        textoNome = new JTextField("");
-        contraseña = new JPasswordField("contraseña");
-        areaTexto = new JTextArea("");
-        lista = new JList();
-        
-         marco.setSize(900, 600);
-         marco.setVisible(true);
-         marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         
-         panel.setLayout(null);
-         panel.setSize(30, 20);
-         
-         panel2.setSize(30,200);
-         
-         panel3.setSize(400,200);
-         
-         
-         textoNome.setBounds(120, 120, 300, 20);
-         textoNome.setText("");
-         contraseña.setBounds(120, 170, 300, 20);
-         contraseña.setText("");
-         
-         etiqueta.setText("nome");
-         etiqueta2.setText("password");
-         etiqueta.setBounds(50, 100, 100, 50);
-         etiqueta2.setBounds(50, 150, 100, 50);
-        
-        
-         boton.setBounds(100, 200, 100, 50);
-         boton2.setBounds(250, 200, 100,50);
-         
-        
-         
-         panel3.add(textoNome);
-         panel3.add(contraseña);
-         panel3.add(etiqueta);
-         panel3.add(etiqueta2);
-         panel3.add(boton);
-         panel3.add(boton2);
-         marco.add(panel);
-         marco.add(panel2);
-        
-         
-        
+
+        //caracteristicas
+        marco.setSize(600, 800);
+        panel.setSize(600, 800);
+       
     }
+
+    public void instanciarPanel1() {
+        panel1 = new JPanel();
+        bPremer = new JButton("PREMER");
+        bLimpar = new JButton("LIMPAR");
+        Nome = new JLabel("NOME ");
+        NomeText = new JTextField();
+        Password = new JLabel("PASSWORD ");
+        Passwordtext = new JPasswordField(6);
+
+        panel1.setSize(250, 300);
+        panel1.setBackground(Color.red);
+
+    }
+
+    public void instanciarPanel2() {
+        panel2 = new JPanel();
+        boton = new JButton("boton");
+        atComentarios = new JTextArea("", 20, 10);
+        Items = new JList(items);
+
+        panel2.setSize(250, 300);
+        panel2.setBackground(Color.BLUE);
+    }
+
+    public void crearVentana() {
+        instanciarComponentesVentana();
+        ordenarPanel1();
+        ordenarPanel2();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        panel.add(panel1);
+        panel.add(panel2);
+        bLimpar.addActionListener(this);
+        boton.addActionListener(this);
+        marco.add(panel);
+
+        marco.setVisible(true);
+        marco.setDefaultCloseOperation(3);
+
+    }
+
+    public void ordenarPanel1() {
+        instanciarPanel1();
+        panel1.add(Nome);
+        panel1.add(NomeText);
+
+        panel1.add(Password);
+        panel1.add(Passwordtext);
+
+        panel1.add(bPremer);
+        panel1.add(bLimpar);
+
+    }
+
+    public void ordenarPanel2() {
+        instanciarPanel2();
+        panel2.add(Items);
+        panel2.add(atComentarios);
+        panel2.add(boton);
+
+    }
+
     
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == bLimpar) {
+            //poner en blanco todos los campos
+            NomeText.setText("");
+            Passwordtext.setText("");
+            atComentarios.setText("");
+            Items.clearSelection();
+        } else if (e.getSource() == bPremer) {
+
+        } else if (e.getSource() == boton) {
+            if (Items.isSelectionEmpty() == true) {
+                atComentarios.setText(NomeText.getText()
+                        + "\nCurso sin seleccionar");
+            } else {
+                atComentarios.setText(NomeText.getText() + "\n"
+                        + Items.getSelectedValue());
+
+            }
+        }
+    }
+
     
+       
+    
+
 }
